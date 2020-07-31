818e4e9edd706cf755953e3dd7505154a09e0a9e
Ignacio Sanchez <nachoandres_sanchezb@hotmail.com>
2020-07-31 16:49:20 -0400

    Added way to use GPU

--- Image_Classifier_Tutorial/Image_classifier.py
+++ Image_Classifier_Tutorial/Image_classifier.py
@@ -84,6 +84,11 @@ class Net(nn.Module):
 
 net = Net()
 
+### SEND NET TO GPU IF AVAILABLE
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+net.to(device)
+
 ### DEFINE LOSS FUNCTION AND OPTIMIZER
 
 import torch.optim as optim
@@ -93,18 +98,21 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 ### TRAIN THE NETWORK
 
-for epoch in range(2):  # loop over the dataset multiple times
+num_epochs = 2
+
+for epoch in range(num_epochs):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        #inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        outputs = net(inputs)
+        outputs = net(inputs).to(device)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -121,16 +129,17 @@ print('Finished Training')
 ### TEST THE NETWORK
 
 dataiter = iter(testloader)
-images, labels = dataiter.next()
+dataiter_next = dataiter.next()
+images, labels = dataiter_next[0].to(device), dataiter_next[1].to(device)
 
 # print images
-imshow(torchvision.utils.make_grid(images))
+imshow(torchvision.utils.make_grid(images).cpu())
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-outputs = net(images)
+outputs = net(images).to(device)
 _, predicted = torch.max(outputs, 1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
@@ -142,8 +151,8 @@ correct = 0
 total = 0
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
-        outputs = net(images)
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images).to(device)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
