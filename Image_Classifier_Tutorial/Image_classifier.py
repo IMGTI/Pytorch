2b1c6e8f8d6c8876fd226bea8e5466dd064e8cbf
Ignacio Sanchez <nachoandres_sanchezb@hotmail.com>
2020-08-06 11:21:27 -0400

    Added some code and new state dict

--- Image_Classifier_Tutorial/Image_classifier.py
+++ Image_Classifier_Tutorial/Image_classifier.py
@@ -134,9 +134,16 @@ for epoch in range(num_epochs):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+            #print('[%d, %5d] loss: %.3f' %
+            #      (epoch + 1, i + 1, running_loss / 2000))
+
+            # Store mean running loss of 2000 mini-batches
+            final_mean_running_loss = running_loss / 2000
+
             running_loss = 0.0
+
+# Print final statistics
+print('Final Mean Running Loss = ', final_mean_running_loss)
 
 print('Finished Training')
 
