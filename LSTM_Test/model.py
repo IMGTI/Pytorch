bc3980d30ac5046d8070a7fc7b1fcda6854f509c
XxZeroGravityxX <nachoandres_sanchezb@hotmail.com>
2020-10-28 18:28:32 -0300

    Commit for local repo

--- LSTM_Test/model.py
+++ LSTM_Test/model.py
@@ -11,7 +11,6 @@ class LSTM(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
-        self.batch_size = 27
 
         if self.bidirectional:
             self.ways = 2
@@ -26,7 +25,7 @@ class LSTM(nn.Module):
 
         # Send net to GPU if possible
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-'''
+        '''
         pass
 
     def forward(self, x):
@@ -41,12 +40,13 @@ class LSTM(nn.Module):
 
         return out
 
-'''
+        '''
+        self.batch_size = 27
         self.h_0 = Variable(torch.zeros(
                    self.num_layers*self.ways, self.batch_size, self.hidden_size))
         self.c_0 = Variable(torch.zeros(
                    self.num_layers*self.ways, self.batch_size, self.hidden_size))
-
+        print('########',self.h_0.data.size(),self.c_0.data.size())
         pass
 
     def forward(self, x):
@@ -55,9 +55,8 @@ class LSTM(nn.Module):
         #c_0 = Variable(torch.zeros(
         #    self.num_layers*self.ways, x.size(0), self.hidden_size))
         # Propagate input through LSTM
-        ula, (self.h_0, self.c_0) = self.lstm(x, (Variable(self.h_0.data).to(self.device), Variable(self.c_0.data).to(self.device)))
-
+        ula, (self.h_0, self.c_0) = self.lstm(x, (Variable(self.h_0.detach()).to(self.device), Variable(self.c_0.detach()).to(self.device)))
+        print(ula.size(),self.h_0.data.size(),self.c_0.data.size())
         out = self.fc(ula[:,-1,:])
-
+        print('########2',out.size())
         return out
-'''
