d5dee90b5a1c577159dbfa1df376c10a9503ebde
XxZeroGravityxX <nachoandres_sanchezb@hotmail.com>
2020-12-15 11:04:51 -0300

    Testing data augmentation by scaling factor

--- LSTM_Test/bootstrap.py
+++ LSTM_Test/bootstrap.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import sys
 import getopt
+from pandas import ExcelWriter
 
 ### Set RNG seeds
 
@@ -33,14 +34,54 @@ def arg_parser(argv):
     return mode
 
 if __name__ == "__main__":
-   mode = arg_parser(sys.argv[1:])
+    mode = arg_parser(sys.argv[1:])
+
+### Data augmentation
+
+def augment_data(file_name, source_folder_name, target_folder_name):
+    # Create folder if does not exist and copy original file into it
+    try:
+        os.mkdir(target_folder_name)
+        os.system("cp " + source_folder_name + "/" + file_name +
+                  " " + target_folder_name + "/" + file_name)
+    except:
+        pass
+    # Read figures from excel
+    data = pd.read_excel(source_folder_name + '/' + file_name, usecols=[0,ind], names=['times', 'defs'])
+
+    # Scale with random coefficient
+    sc_coef = np.random.random()
+
+    data['defs'] *= sc_coef
+
+    # Create new excel with figures
+
+    writer = ExcelWriter(source_folder_name + '/' + file_name[:-5] + '_sc_' + str(sc_coef) + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
+
+    data.to_excel(writer, index=False)
+    writer.save()
+    pass
 
 
 # Main data folder
 data_folder = 'datos/1_All_data'
 
-# Sub data data folders
+# Sub data folders
 data_folders = np.array(os.listdir(data_folder))
+
+if mode=='Train':
+    # Folder's names
+    source_folder_name = data_folder
+    target_folder_name = 'datos/1_All_data_augmented'
+    # Agument data
+    for file_name in data_folders:
+        ind = 1
+        while ind<=5:
+            augment_data(file_name, source_folder_name , target_folder_name)
+            ind += 1
+    data_folders = np.array(os.listdir(target_folder_name))
+
+# Shuffle files in data folders
 np.random.shuffle(data_folders)
 
 if mode=='Train':
