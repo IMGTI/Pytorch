305bac77095240e566a750e60070003ddd3a86e4
XxZeroGravityxX <nachoandres_sanchezb@hotmail.com>
2020-12-15 11:58:29 -0300

    Fixed some errors in managing folders

--- LSTM_Test/bootstrap.py
+++ LSTM_Test/bootstrap.py
@@ -3,6 +3,8 @@ import os
 import sys
 import getopt
 from pandas import ExcelWriter
+import pandas as pd
+from tqdm import tqdm
 
 ### Set RNG seeds
 
@@ -12,8 +14,9 @@ np.random.seed(seed=seed)
 ### Parse line arguments
 def arg_parser(argv):
     mode = 'Train'
+    aug = False
     try:
-        opts, args = getopt.getopt(argv,"hm:",["mode="])
+        opts, args = getopt.getopt(argv,"hm:a:",["mode=", "augment="])
     except getopt.GetoptError:
         print('argparser.py -m <Train/Test>')
         sys.exit(2)
@@ -21,20 +24,25 @@ def arg_parser(argv):
         if opt == '-h':
             print('------------ Commands -------------')
             print('-m: Mode (Train or Test)')
+            print('-a: Augment Data (True or False)')
             print('-h: Show help')
             print('--------- Example usage ----------')
             print('For training:')
-            print('argparser.py -m Train')
+            print('argparser.py -m Train -a <True/False>')
             print('For testing:')
-            print('argparser.py -m Test')
+            print('argparser.py -m Test -a <True/False>')
             sys.exit()
         elif opt in ("-m", "--mode"):
             mode = arg
-
-    return mode
+        elif opt in ("-a", "--augment"):
+            if arg=='True':
+                aug = True
+            else:
+                aug = False
+    return mode, aug
 
 if __name__ == "__main__":
-    mode = arg_parser(sys.argv[1:])
+    mode, aug = arg_parser(sys.argv[1:])
 
 ### Data augmentation
 
@@ -42,12 +50,17 @@ def augment_data(file_name, source_folder_name, target_folder_name):
     # Create folder if does not exist and copy original file into it
     try:
         os.mkdir(target_folder_name)
-        os.system("cp " + source_folder_name + "/" + file_name +
-                  " " + target_folder_name + "/" + file_name)
     except:
         pass
+
     # Read figures from excel
     data = pd.read_excel(source_folder_name + '/' + file_name, usecols=[0,ind], names=['times', 'defs'])
+
+    # Copy file from source to target
+    if file_name not in os.listdir(target_folder_name):
+        writer = ExcelWriter(target_folder_name + '/' + file_name[:-5] + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
+        data.to_excel(writer, index=False)
+        writer.save()
 
     # Scale with random coefficient
     sc_coef = np.random.random()
@@ -56,7 +69,7 @@ def augment_data(file_name, source_folder_name, target_folder_name):
 
     # Create new excel with figures
 
-    writer = ExcelWriter(source_folder_name + '/' + file_name[:-5] + '_sc_' + str(sc_coef) + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
+    writer = ExcelWriter(target_folder_name + '/' + file_name[:-5] + '_sc_' + str(sc_coef) + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
 
     data.to_excel(writer, index=False)
     writer.save()
@@ -65,21 +78,24 @@ def augment_data(file_name, source_folder_name, target_folder_name):
 
 # Main data folder
 data_folder = 'datos/1_All_data'
+augmented_data_folder = 'datos/1_All_data_augmented'
 
 # Sub data folders
 data_folders = np.array(os.listdir(data_folder))
 
-if mode=='Train':
+if aug==True:
     # Folder's names
     source_folder_name = data_folder
-    target_folder_name = 'datos/1_All_data_augmented'
+    target_folder_name = augmented_data_folder
     # Agument data
-    for file_name in data_folders:
+    print('Beginning Data Augmentation...')
+    for file_name in tqdm(data_folders, total=len(data_folders)):
         ind = 1
         while ind<=5:
             augment_data(file_name, source_folder_name , target_folder_name)
             ind += 1
     data_folders = np.array(os.listdir(target_folder_name))
+    data_folder = augmented_data_folder
 
 # Shuffle files in data folders
 np.random.shuffle(data_folders)
