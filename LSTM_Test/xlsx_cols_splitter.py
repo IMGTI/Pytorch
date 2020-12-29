7eedebc3abf29e0b4e9e88d8ca9082d8b4bb7005
XxZeroGravityxX <nachoandres_sanchezb@hotmail.com>
2020-12-29 11:51:33 -0300

    Made new function to separate "real data" in different datasets. This is separate deformation data in different datasets when it returns to 0

--- LSTM_Test/xlsx_cols_splitter.py
+++ LSTM_Test/xlsx_cols_splitter.py
@@ -2,21 +2,38 @@ import pandas as pd
 import numpy as np
 from pandas import ExcelWriter
 
+def ext_mounts(data):
+    # Divide data per mounts (when def is 0, create another dataset)
+    ind_data = np.where(data.iloc[:,1].values==0)[0]
+    datasets = {}
+    for ind, val in enumerate(ind_data):
+        if ind!=(len(ind_data)-1):
+            datasets[ind] = data.iloc[ind_data[ind]:ind_data[ind+1]].reset_index(drop=True)
+        else:
+            datasets[ind] = data.iloc[ind_data[ind]:].reset_index(drop=True)
+    return datasets
+
 def ext_data(file_name):
     ind = 1
     while True:
         try:
             # Read figures from excel
             data = pd.read_excel(file_name, usecols=[0,ind], names=['times', 'defs'])
-            if np.isnan(data['defs'].values[0]) or data['defs'].values[0]=='':
+
+            # Extract mounts (erase artifacts)
+            datasets = ext_mounts(data)
+
+            # If datasets is empty, then there is no data, so break
+            if datasets=={}:
                 break
 
-            # Create new excel with figures
+            for ind_data, data in datasets.items():
+                # Create new excel with figures
 
-            writer = ExcelWriter(file_name[:-5] + '_fig_' + str(ind) + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
+                writer = ExcelWriter(file_name[:-5] + '_fig_' + str(ind) + '_data_' + str(ind_data) + '.xlsx',  datetime_format='dd-mm-yy hh:mm')
 
-            data.to_excel(writer, index=False)
-            writer.save()
+                data.to_excel(writer, index=False)
+                writer.save()
 
 
             ind += 1
