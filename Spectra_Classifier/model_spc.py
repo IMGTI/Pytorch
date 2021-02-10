Added softmax layer, fixed bugs related to labels torch.type, f1score and others

Spectra_Classifier/model_spc.py
@@ -46,6 +46,8 @@ class CNN(nn.Module):
                                                            # (stride=1, 3 times, one for each conv+pool)
         self.fc2 = nn.Linear(32, self.num_classes)
 
+        self.soft = nn.Softmax(dim=1)
+
     def forward(self, x, hidden=None):
         x = x.view(-1, 1, x.size()[1])  # Change input shape for in_channels=1
                                         # x.size()[1]==window_size
@@ -55,6 +57,6 @@ class CNN(nn.Module):
         x = self.pool(F.relu(self.conv3(x)))
         x = x.view(-1,self.filters_number*self.l_fc)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.soft(self.fc2(x))
 
         return x

Spectra_Classifier/test_spc.py
@@ -54,9 +54,9 @@ class Test(object):
         if len(result)==1:
             results_file.write('Recall  = ' + str(result) + '\n')
         else:
-            results_file.write('Precision  = ' + str(result[0]) + '\n')
-            results_file.write('Recall  = ' + str(result[1]) + '\n')
-            results_file.write('F1 Score  = ' + str(result[2]) + '\n')
+            results_file.write('Precision (Median) = ' + str(result[0]) + '\n')
+            results_file.write('Recall = ' + str(result[1]) + '\n')
+            results_file.write('F1 Score (Micro Average) = ' + str(result[2]) + '\n')
         results_file.close()
 
     def test_model(self, amp, label):
@@ -72,7 +72,8 @@ class Test(object):
 
             correct = 0
             total = 0
-            f1_score = []
+            f1_labels = []
+            f1_predicted = []
             precision = []
             with torch.no_grad():
                 for ind, input in enumerate(amp):
@@ -89,21 +90,19 @@ class Test(object):
 
                     total += 1#labels.size(0)
 
-                    # Store F1 score
-                    print('#####', np.array(label)[ind], np.array(outputs.cpu())[0])
-                    f1_score.append(f1s(np.array(label)[ind], np.array(outputs.cpu())[0]))
-                    # Store precision
+                    # Store for median precision
                     precision.append(np.array(_.cpu())[0])
+                    # Store for F1 score
+                    f1_labels.append(labels)
+                    f1_predicted.append(predicted)
 
-            f1_score = np.array(f1_score)
-            precision = np.array(precision)
 
             print('Accuracy of the network on whole dataset (Recall): %d %%' % (
                 100 * correct / total))
 
-            self.save_results([np.median(precision),
+            self.save_results([np.median(np.array(precision)),
                                100 * correct / total,
-                               np.median(f1_score)])
+                               f1s(f1_labels, f1_predicted, average='micro')])
 
         ### Data Test (Classification)
         except:

Spectra_Classifier/train_spc.py
@@ -67,6 +67,11 @@ class Train(object):
 
     def train_model(self, batch_size, learning_rate, num_epochs, amp, label,
                     spc=None, b=0.9, method='ens', validate=True, patience=10):
+        # Make sure label (target) tensor are torch.int64 or torch.long
+        # and it contains class indices (0 or 1 or 2) instead of one-hot encoded vectors
+        # Transform one-hot encoded vectors
+        _, label = torch.max(label, 1)
+
         # Initialize the early stopping object
         early_stopping = EarlyStopping(patience=patience, verbose=True)
 
@@ -84,8 +89,9 @@ class Train(object):
 
         if spc==None:
             self.criterion = torch.nn.CrossEntropyLoss()  # for classification
-        self.optimizer = torch.optim.Adam(self.cnn.parameters(), lr=learning_rate)
-
+        #self.optimizer = torch.optim.Adam(self.cnn.parameters(), lr=learning_rate)
+        momentum = 0.9
+        self.optimizer = torch.optim.SGD(self.cnn.parameters(), lr=learning_rate, momentum=momentum)
         # Try to load the model and optimizer 's state dictionaries
         self.load_model()
 